df2f5b659ee057701708a7368d0116fd7a333117
jeena <jeena2284@gmail.com>
2026-06-22 09:44:10 +0530

    refactor: remove legacy migration scripts and maintenance tools from server directory

--- check_logs.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-
-async function main() {
-  console.log("AuditLogs:", await prisma.auditLog.findMany({ where: { action: "FORGOT_PASSWORD_REQUEST" } }));
-  console.log("Notifications:", await prisma.notification.findMany({ where: { type: "PASSWORD_RESET" } }));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
--- check_pass.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function main() {
-  const isMatch = await bcrypt.compare('pon', '$2b$10$LggSlKbp82P8mJGzJe9mue/ThtiaElpmO.IUef8nxTp3Gq8BqNZae');
-  console.log('Matches pon:', isMatch);
-}
-
-main();
